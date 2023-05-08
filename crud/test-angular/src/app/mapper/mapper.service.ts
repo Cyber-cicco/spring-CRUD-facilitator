5406368c2318aca28b852a481ead12b08b2151cb
Abel Ciccoli <abelcoli@outlook.fr>
2023-05-08 22:22:31 +0200

    feat(modif): le formulaire est maintenant autoremplie avec tous les champs des entités. Petits changements pour optimiser le mapper des champs du tableau

--- crud/test-angular/src/app/mapper/mapper.service.ts
+++ crud/test-angular/src/app/mapper/mapper.service.ts
@@ -10,24 +10,12 @@ export class MapperService {
     this.mapChamps.set('id', 'Identifiant');
     this.mapChamps.set('emailClient', 'E-mail du client');
     this.mapChamps.set('emailLivreur', 'E-mail du liveur');
-    this.mapChamps.set('pizza', 'Pizza');
-    this.mapChamps.set('menu', 'Menu');
-    this.mapChamps.set('status', 'Status');
     this.mapChamps.set('dateCommande', 'Date de création');
     this.mapChamps.set('nomMagasin', 'Pizzeria');
-    this.mapChamps.set('adresse', 'Adresse');
-    this.mapChamps.set('prix', 'Prix');
-    this.mapChamps.set('nom', 'Nom');
-    this.mapChamps.set('prenom', 'Prenom');
     this.mapChamps.set('email', 'E-Mail');
-    this.mapChamps.set('version', 'Version');
     this.mapChamps.set('ingredients', 'Composition');
-    this.mapChamps.set('toppings', 'Toppings');
-    this.mapChamps.set('pate', 'Pate');
-    //this.mapChamps.set('', '');
-    //this.mapChamps.set('', '');
-    //this.mapChamps.set('', '');
-    //this.mapChamps.set('', '');
-    //this.mapChamps.set('', '');
+    this.mapChamps.set('motDePasse', 'Mot de passe');
+    this.mapChamps.set('codePostal', 'Code Postal');
+    this.mapChamps.set('', '');
   }
 }
