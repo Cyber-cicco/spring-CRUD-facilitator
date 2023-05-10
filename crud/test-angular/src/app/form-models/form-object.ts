7e72e630e9bd0690756a606a428b8a5b70c19112
Abel Ciccoli <abelcoli@outlook.fr>
2023-05-10 21:18:44 +0200

    feat(modif): les valeurs du formulaires sont maintenant bien récupérées une fois changées.

--- crud/test-angular/src/app/form-models/form-object.ts
+++ crud/test-angular/src/app/form-models/form-object.ts
@@ -2,9 +2,10 @@ import {ValidatorFn} from "@angular/forms";
 import {FormType} from "./form-type-enum";
 import {FormOption} from "./form-option-enum";
 import { Multichoice } from "./form-mutlichoice";
+import { FormValue } from "./form-value";
 
 export interface FormObject {
-  value:string | string[] | Date | number | Multichoice[]
+  value:FormValue | Multichoice[]
   validators:ValidatorFn | ValidatorFn[] | undefined
   type:FormType
   options:FormOption[]
--- crud/test-angular/src/app/shared/modal/modal-modif/modal-modif.component.ts
+++ crud/test-angular/src/app/shared/modal/modal-modif/modal-modif.component.ts
@@ -7,6 +7,7 @@ import {TransferFormObject} from "../../../models/transfer-form-object";
 import {FormOption} from "../../../form-models/form-option-enum";
 import { FormType } from 'src/app/form-models/form-type-enum';
 import { Multichoice } from 'src/app/form-models/form-mutlichoice';
+import { FormValue } from 'src/app/form-models/form-value';
 
 /**
  * Modal permettant de récupérer les champs de l'entité qu'elle est censé créer / modifier
@@ -23,13 +24,21 @@ export class ModalModifComponent implements OnInit, OnDestroy{
 
   /**Map des champs du formulaire et des ses propriétés*/
   items:TransferFormObject[] = [];
+
   /**Souscription à au sujet offrant les champs du formulaire à remplir*/
-  modifSubscription:Subscription = new Subscription();
+  private modifSubscription:Subscription = new Subscription();
+
   /**Fields du formulaire nécessitant d'être récupérés auprès de l'API*/
   asyncFields = new Map<string, string[]>();
+
   /**Formulaire*/
   formModification: FormGroup;
-  checkBoxValues:Map<string, string[]> = new Map();
+
+  boxValues:Map<string, Map<string,boolean>> = new Map();
+
+  inputValues : Map<string, FormValue | Multichoice[]> = new Map();
+
+
   constructor(private crud:CrudDataflowService,
               private fb:FormBuilder,
               private modalService:MatDialog) {
@@ -46,60 +55,93 @@ export class ModalModifComponent implements OnInit, OnDestroy{
 
   /**Création des champs du formulaire*/
   ngOnInit(): void {
+
     this.asyncFields = this.crud.getAsyncFieldsSubscriptions().getValue()
     this.modifSubscription = this.crud.getModifSubject().subscribe(value => {
-      console.log("subscription received");
-      console.log(value);
-      console.log(this.formModification);
+
       if(value != undefined){
+
         this.items = value;
         const formGroupFields:any = {};
+
         for(let tfo of value){
-            if (tfo.form.type != FormType.MULTICHOICE){
-              console.log("not in multichoice")
-              console.log(tfo.name);
+            if (tfo.form.type != FormType.MULTICHOICE && tfo.form.type != FormType.CHECKBOX){
+
               let control =new FormControl({value:tfo.form.value, disabled:tfo.form.options.includes(FormOption.READONLY)});
               if (tfo.form.validators != undefined) control.addValidators(tfo.form.validators);
               formGroupFields[tfo.name] = control;
+              this.inputValues.set(tfo.name, tfo.form.value);
+
+
             } else if(tfo.form.value instanceof Array){
-              console.log("in multichoice");
-              for(let option of this.asyncFields.get(tfo.name)!){
-                let control = new FormControl({});
-                formGroupFields[option] = control;
-                for(let multichoice of tfo.form.value){
-                  console.log(tfo.form.value);
-                  console.log(multichoice);
-                  console.log(this.instanceOfMultichoice(multichoice));
-                  if(this.instanceOfMultichoice(multichoice)){
-                    console.log(multichoice.nom);
-                    console.log(multichoice.nom === option);
-                    if(multichoice.nom === option){
-                      formGroupFields[option].value = multichoice.nb;
+
+              if(tfo.form.type === FormType.CHECKBOX && tfo.form.value instanceof Array){
+                let boxMap = new Map<string,boolean>();
+                if(!this.asyncFields.has(tfo.name)) throw "Les valeurs possibles d'une checkbox n'ont pas été renseignées";
+
+                for(let box of this.asyncFields.get(tfo.name)!){
+                  formGroupFields[box] = new FormControl();
+                  boxMap.set(box, false);
+                }
+
+                for(let checkedBox of tfo.form.value){
+                  boxMap.set(String(checkedBox), true);
+
+                }
+
+                this.boxValues.set(tfo.name, boxMap);
+
+              } else {
+                for(let option of this.asyncFields.get(tfo.name)!){
+                  let control = new FormControl(option);
+                  formGroupFields[option] = control;
+
+                  for(let multichoice of tfo.form.value){
+                    if(this.instanceOfMultichoice(multichoice)){
+                      if(multichoice.nom === option){
+                        formGroupFields[option].value = multichoice.nb;
+                      }
                     }
                   }
                 }
               }
+
             }
         }
         this.formModification = this.fb.group(formGroupFields);
-        console.log("modal-modif")
-        console.log(this.formModification.controls);
-        console.log(this.asyncFields);
       }
     });
 
   }
   /**Foncition appelée lors du clique sur un bouton fermant la modale
-   * TODO: implémenter la création de la nouvelle map à partir des champs du formulaire
    * */
   closeModal(validated: boolean) {
     if(validated){
+
       console.log("modal moif validated");
-      for(let [key, value] of Object.entries(this.formModification.controls)){
-        console.log(key);
-        console.log(value.value) ;
+      let mapResult = new Map<string, FormValue | Multichoice[]>();
+
+      for(let tfo of this.items){
+
+        if (![FormType.MULTICHOICE, FormType.CHECKBOX, FormType.RADIO, FormType.SELECT].includes(tfo.form.type)){
+            mapResult.set(tfo.id, this.formModification.controls[tfo.name]?.value);
+
+        } else if (tfo.form.type == FormType.CHECKBOX){
+
+            let arraySult:string[] = []
+
+            for(let subEntity of this.boxValues.get(tfo.name)!){
+              if(subEntity[1]) arraySult.push(subEntity[0]);
+            }
+
+            mapResult.set(tfo.id, arraySult);
+          } else {
+
+            mapResult.set(tfo.id, this.inputValues.get(tfo.name)!);
+          }
+        }
+        console.log(mapResult);
       }
-    }
     this.modalService.closeAll();
   }
 
@@ -109,4 +151,18 @@ export class ModalModifComponent implements OnInit, OnDestroy{
     }
     return item === option;
   }
+
+  addBoxValues(field:string, value:string){
+    this.boxValues.get(field)!.set(value, !this.boxValues.get(field)!.get(value));
+    console.log(this.boxValues);
+  }
+
+  addInputValues(field:string, value:string){
+    this.inputValues.set(field, value);
+  }
+
+  addSelectValues(field:string ,$event:any){
+    this.addInputValues(field, $event.target.value);
+  }
+
 }
